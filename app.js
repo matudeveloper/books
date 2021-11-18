d062bb412b405fd6d6ade995a07ac354bcfc9297
martin <martin.laan@khk.ee>
2021-11-18 20:58:39 +0200

    JS Books

--- app.js
+++ app.js
@@ -8,54 +8,6 @@ const delTasksBtn = document.querySelector('#del-tasks');
 form.addEventListener('submit', addBook);
 tasksList.addEventListener('click', deleteTask);
 delTasksBtn.addEventListener('click', deleteTasks);
-document.addEventListener('DOMContentLoaded', getBooks);
-
-function getBooks(){
-    let books;
-    if(localStorage.getItem('books') === null){
-        books = [];
-    } else {
-        books = JSON.parse(localStorage.getItem('books'));
-    }
-    //console.log(books);
-    for(let i = 0; i < books.length; i++){
-        let book = books[i];
-
-        //console.log(book);
-        const li = document.createElement('tr');
-        let text;
-        for(let j = 0; j < book.length; j++){
-
-            li.classname = "collection-item";
-
-            text = `<td>${book[0]}</td><td>${book[1]}</td><td>${book[2]}</td>`;
-        }
-
-        li.innerHTML += text;
-        //console.log(books);
-
-        td = document.createElement('td');
-
-        const link = document.createElement('a');
-
-        link.setAttribute('href', '#');
-
-        link.className = 'secondary-content';
-
-        link.appendChild(document.createTextNode('X'));
-
-        td.appendChild(link);
-
-        const ul = document.querySelector('.collection');
-
-        li.appendChild(td);
-
-        ul.appendChild(li);
-
-
-
-    }
-}
 
 function deleteTasks(e)
 {
@@ -63,12 +15,6 @@ function deleteTasks(e)
     {
         tasksList.removeChild(tasksList.firstChild)
     }
-    if(localStorage.getItem('books')===null)
-    {
-        let books = [];
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-    localStorage.removeItem('books');
 }
 
 function deleteTask(e)
@@ -76,33 +22,9 @@ function deleteTask(e)
     if (e.target.textContent == 'X')
     {
         if (confirm("do you want to delete this task ?")) {
-            let Xtarget = e.target.parentElement;
-            Xtarget.parentElement.remove();
-            let bookitem = Xtarget.parentElement.firstChild.textContent;
-            //console.log(bookitem);
-            deleteBook(bookitem);
+            e.target.parentElement.remove();
         }
     }
-}
-
-function deleteBook(bookitem)
-{
-    let books;
-    let ISBNValue = bookISBN.value;
-    if(localStorage.getItem('books') === null){
-        books = [];
-    } else {
-        books = JSON.parse(localStorage.getItem('books'));
-    }
-
-    books.forEach(function (book, index){
-        console.log(bookitem);
-        if(book[0] === bookitem){
-            books.splice(index, 1);
-        }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
 }
 
 function addBook(e)
@@ -133,11 +55,6 @@ function addBook(e)
 
     ul.appendChild(li);
 
-
-    const book = [bookAuthorVal, bookTitleVal, bookISBNVal]
-
-    BookToLocalStorage(book);
-
     //taskInput.value = '';
     bookAuthor.value = '';
     bookTitle.value = '';
@@ -145,17 +62,4 @@ function addBook(e)
 
     e.preventDefault();
 
-}
-
-
-
-function BookToLocalStorage(book){
-    let books;
-    if(localStorage.getItem('books') === null){
-        books = [];
-    } else {
-        books = JSON.parse(localStorage.getItem('books'));
-    }
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
 }
